e7b37fd82078f57f81469a693edcd43231761663
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-09-26 10:43:05 +0100

    add first

---
+++ src/Components/board.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./board.css" 
+import { Box } from "./Components/box"
+
+export const Board = ({Board}) => {
+    
+        return (
+
+       <div>
+        <Box value="X" onClick={null} /></div>
+    )
+    
+    
+    
+    }
